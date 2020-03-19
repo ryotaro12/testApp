a3249b8e10f60e20e9d89132da27ab2ed9fa5e9a
ryotaro12 <sidetechtr@gmail.com>
2020-03-19 21:25:36 +0900

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Tsuji Ryotaro on 2020/03/19.
+//  Copyright © 2020 Tsuji Ryotaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
